7a2ff0404411b95ebebb242d795c3f0b1431c82f
Tom Witkowski <dev.gummibeer@gmail.com>
2020-07-21 17:36:27 +0200

    add migration

--- src/AddressableServiceProvider.php
+++ src/AddressableServiceProvider.php
@@ -3,20 +3,25 @@
 namespace Elbgoods\LaravelAddressable;
 
 use Elbgoods\LaravelAddressable\Facades\AddressFormats;
-use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class AddressableServiceProvider extends ServiceProvider implements DeferrableProvider
+class AddressableServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        if($this->app->runningInConsole()) {
+            if (! class_exists('CreateAddressesTable')) {
+                $timestamp = date('Y_m_d_His', time());
+
+                $this->publishes([
+                    __DIR__.'/../migrations/create_addresses_table.php.stub' => database_path("/migrations/{$timestamp}_create_activity_log_table.php"),
+                ], 'migrations');
+            }
+        }
+    }
+
     public function register(): void
     {
         $this->app->singleton(AddressFormats::class);
-    }
-
-    public function provides(): array
-    {
-        return [
-            AddressFormats::class,
-        ];
     }
 }
